d56d65b52887f452fbcc4917844b0f83fa7490a5
oleg <98418088+etoOleh@users.noreply.github.com>
2022-12-02 13:19:06 +0300

    Add files via upload

---
+++ BUYSELL(avito copy) version 0/src/main/java/com/example/buysell/controllers/ProductController.java
@@ -0,0 +1,61 @@
+package com.example.buysell.controllers;
+
+import com.example.buysell.models.Product;
+import com.example.buysell.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.Principal;
+
+@Controller
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping("/")
+    public String products(@RequestParam(name = "title", required = false) String title, Principal principal, Model model) {
+        model.addAttribute("products", productService.listProducts(title));
+        model.addAttribute("user", productService.getUserByPrincipal(principal));
+        return "products";
+    }
+
+    @GetMapping("/product/{id}")
+    public String productInfo(@PathVariable Long id, Model model) {
+        Product product = productService.getProductById(id);
+        model.addAttribute("product", product);
+        model.addAttribute("images", product.getImages());
+        return "product-info";
+    }
+
+    @PostMapping("/product/create")
+    public String createProduct(@RequestParam("file1") MultipartFile file1, @RequestParam("file2") MultipartFile file2,
+                                @RequestParam("file3") MultipartFile file3, Product product, Principal principal) throws IOException {
+        productService.saveProduct(principal, product, file1, file2, file3);
+        return "redirect:/";
+    }
+
+    @PostMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return "redirect:/";
+    }
+
+//    @GetMapping(value="/css/{cssFile}")
+//    public @ResponseBody byte[] getFile(@PathVariable("cssFile") String cssFile) throws IOException {
+//
+//        InputStream in = getClass()
+//                .getResourceAsStream("/static/css/" + cssFile);
+//        try {
+//            return in.readAllBytes();
+//
+//        } catch (Exception e){
+//            var error = new String("ERROR: css file (/css/" + cssFile + ") not found");
+//            return error.getBytes();
+//        }
+//    }
+}
